e2e4d449fe19c4ae03ba7bfe712152a906cd3ff0
kong Luo Xing <jayklx@gmail.com>
2013-03-13 15:27:18 +0800

    reconstructed

--- ta.py
+++ ta.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 #coding:utf-8
+#TODO: logging support
 from pyh import *
-import types
-import ConfigParser, time
+import ConfigParser
+import time
 import telnetlib
 import re
 #import logging
@@ -24,7 +25,7 @@ class Connector(object):
         config.read(__config__)
         if config.get('os', 'telnet') == '1':
             self.TELNET = True
-            self.PROMPT = config.get('os', 'prompt')
+            self.PROMPT = re.compile(config.get('os', 'prompt'))
         self.IPADDR = config.get('os', 'ipaddr')
         self.PORT = config.getint('os', 'port')
         self.USERNAME = config.get('os', 'username')
@@ -34,6 +35,7 @@ class Connector(object):
         self.DBPASSWORD = config.get('db', 'dbpassword')
         self.EXTRA_ARGS = config.get('db', 'extra_args')
         self.conn = self.remote_init()
+
     def check_rpm(self, rpm_name):
         rpms = self.remote_exec('rpm -qa')
 #        print rpms
@@ -43,6 +45,7 @@ class Connector(object):
         else:
             print u'未安装' + rpm_name + u'包，请安装后重试'
             return False
+
     def telnet_init(self):
         enter_key = '\n'
         host = self.IPADDR
@@ -51,9 +54,9 @@ class Connector(object):
         password = self.PASSWORD
         tn = telnetlib.Telnet(host, port)
         tn.write(enter_key)
-        ret = tn.read_until('login', 2)
+        tn.read_until('login', 2)
         tn.write(account + enter_key)
-        ret = tn.read_until('assword:', 2)
+        tn.read_until('assword:', 2)
         tn.write(password + enter_key)
         tn.read_until(self.PROMPT)
         return tn
@@ -62,6 +65,7 @@ class Connector(object):
         #print ret
         #tn.write('exit\r\n')
         #tn.close()
+
     def remote_init(self):
         '''初始化ssh连接
 
@@ -76,10 +80,11 @@ class Connector(object):
             self.TELNET = True
             return tn
         else:
-            ssh_client =  paramiko.SSHClient()
+            ssh_client = paramiko.SSHClient()
             ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             try:
-                ssh_client.connect(self.IPADDR, self.PORT, self.USERNAME, self.PASSWORD)
+                ssh_client.connect(self.IPADDR, self.PORT,
+                                   self.USERNAME, self.PASSWORD)
                 return ssh_client
             except:
                 print 'ssh connect false, try telnet'
@@ -91,12 +96,14 @@ class Connector(object):
                 self.TELNET = True
                 print 'connect success'
                 return tn
+
     def remote_exec(self, command, x=0, y=0, info=''):
         '''执行命令
 
         conn: telnet or ssh对象实例'''
+        #TODO: 命令输出写入文件
         conn = self.conn
-        timeout = 2
+#        timeout = 2
         enter_key = '\n'
         if self.TELNET:
             self.conn.write(command + enter_key)
@@ -129,13 +136,15 @@ class Connector(object):
         if info:
             out.insert(1, info)
         return out
+
     def exec_single(self, command):
         '''针对只有一行输出的命令'''
 #        if self.TELNET:
-        return self.remote_exec(command)[1][0].rstrip().lstrip()
+        return self.remote_exec(command)[1][0].strip()
 #        else:
-#            return self.remote_exec(command)[1].rstrip().lstrip()
-    def parse_output(self, output, x = 0, y = 0):
+#            return self.remote_exec(command)[1].strip()
+
+    def parse_output(self, output, x=0, y=0):
         '''转换命令输出为2维list
 
         x表示要删除的行数，y表示要删除的列数(从第一行/列开始)'''
@@ -160,21 +169,25 @@ class Connector(object):
                 for item in list:
                     del item[0]
         return list
+
+
 class OsInfo(object):
     def __init__(self, i_conn):
         self.i_conn = i_conn
+
     def choose(self):
         '''选择操作系统
 
         i_conn: Connect对象实例'''
         s = self.i_conn.exec_single('uname')
         self.os_type = s
-        chooseOS = {'Linux':lambda:self.linux_func(),
-                    'AIX':lambda:self.aix_func(),
-                    'HP-UX':lambda:self.hp_func(),
-                    'Solaris':lambda:self.sol_func()}
+        chooseOS = {'Linux': lambda: self.linux_func(),
+                    'AIX': lambda: self.aix_func(),
+                    'HP-UX': lambda: self.hp_func(),
+                    'Solaris': lambda: self.sol_func()}
         #chooseOS[self.os_type]()
         chooseOS[s]()
+
     def basic_func(self):
         hostname = self.i_conn.exec_single('hostname')
         ipaddr = self.i_conn.exec_single("/sbin/ifconfig -a|grep 'inet addr:'|awk '{print $2}'")[5:]
@@ -182,12 +195,10 @@ class OsInfo(object):
         manufacturer = self.i_conn.remote_exec('/usr/sbin/dmidecode | grep Manufacturer | head -n 1')[1][1].rstrip()
         sn = self.i_conn.exec_single("dmidecode | grep -i 'serial number' | head -n 1 | awk {'print $3'}")
         self.basic_list = ['', '', ['主机名', 'IP 地址', '操作系统', 'CPU架构', '快照时间', '厂商', '序列号'],
-                      [hostname, ipaddr,self.os_type, arch, __time__, manufacturer, sn]]
+                      [hostname, ipaddr, self.os_type, arch, __time__, manufacturer, sn]]
 
     def linux_func(self):
         i_conn = self.i_conn
-        connection.check_rpm('dmidecode')
-        connection.check_rpm('sysstat')
         self.basic_func()
         #系统版本信息
         sys_ver = i_conn.remote_exec('cat /etc/redhat-release')
@@ -196,8 +207,8 @@ class OsInfo(object):
         sys_ver = '.'.join(str(n) for n in sys_ver)
 #        print sys_ver
         kernel = i_conn.exec_single('uname -r')
-        gcc = i_conn.remote_exec('/usr/bin/gcc --version', info = 'GCC')[2][2]
-        version = ['', '', ['系统版本', '内核版本', 'GCC版本', 'Java版本'],[sys_ver, kernel, gcc, java_ver]]
+        gcc = i_conn.remote_exec('/usr/bin/gcc --version', info='GCC')[2][2]
+        version = ['', '', ['系统版本', '内核版本', 'GCC版本', 'Java版本'], [sys_ver, kernel, gcc]]
         #java 版本号获取不到
 #        java = conn.remote_exec('/usr/java/default/bin/java -version', info = 'Java')
 #        print java
@@ -206,10 +217,11 @@ class OsInfo(object):
         for n in java_dir[-1]:
             java_ver += n + ', '
         #获取cpu信息
+
         def cpu_func(matches):
-            cpuinfo = i_conn.remote_exec("grep '" +  matches + "' /proc/cpuinfo | tail -n 1")[1]
+            cpuinfo = i_conn.remote_exec("grep '" + matches + "' /proc/cpuinfo | tail -n 1")[1]
 #            print cpuinfo
-            cpuinfo = cpuinfo[len(cpuinfo)-1:]
+            cpuinfo = cpuinfo[len(cpuinfo) - 1:]
             return cpuinfo[0]
 #        print self.basic_list
 
@@ -219,13 +231,13 @@ class OsInfo(object):
         if cpu_model_t[7] == '@':
             cpu_model = cpu_model_t[6]
         else:
-            cpu_model = cpu_model_t[6]+(cpu_model_t[7])
+            cpu_model = cpu_model_t[6] + (cpu_model_t[7])
         cpu_cores = cpu_func('cores')
         cpu_frq = cpu_func('model')
         cpu_count = cpu_func('processor')
         cpu_list = ['cat /proc/cpuinfo', 'CPU信息',
                     ['制造商', 'BIT', '型号', 'CPU个数', '每CPU核心数', '频率'],
-                    [cpu_manuf, cpu_bit, cpu_model, (int(cpu_count)+1)/int(cpu_cores), cpu_cores, cpu_frq]]
+                    [cpu_manuf, cpu_bit, cpu_model, (int(cpu_count) + 1) / int(cpu_cores), cpu_cores, cpu_frq]]
         mpstat = i_conn.remote_exec('mpstat 2 5', x=2, y=2, info='总CPU使用率')
         mpstat[7].insert(0, 'Average')
 #        print cpu_list
@@ -241,7 +253,7 @@ class OsInfo(object):
         del lvlist[2][4:]
         dflist = i_conn.remote_exec('df -h', info='文件系统信息')
         del dflist[2][6]
-        dflist[2][5]='Mounted on'
+        dflist[2][5] = 'Mounted on'
         #美化df输出
         next = None
         for index, line in enumerate(dflist):
@@ -254,24 +266,24 @@ class OsInfo(object):
         router = i_conn.remote_exec('netstat -rn', x=1, info='内核路由表')
         net_pkg = i_conn.remote_exec('netstat -in', x=1, info='网卡工作状态')
         ipnet = i_conn.remote_exec('ifconfig | grep -A 1 eth | grep inet', y=1, info='IP/掩码')
-        ipnet.insert(2,['IP地址', '广播地址', '子网掩码'])
+        ipnet.insert(2, ['IP地址', '广播地址', '子网掩码'])
 
         iostat = i_conn.remote_exec('iostat', x=5, info='I/O状态')
         vmstat = i_conn.remote_exec('vmstat 2 5', 1, 0, 'vmstat输出')
-        top = i_conn.remote_exec('top -bn 1 | head -n 17', 6, 0, info = 'CPU TOP 10 进程')
+        top = i_conn.remote_exec('top -bn 1 | head -n 17', 6, 0, info='CPU TOP 10 进程')
         mem_top = i_conn.remote_exec("top -bn 1 | sed '1,7 d' | sort -rn -k10 -k6 | head -n 10",
-            info = 'MEM TOP 10进程')
+            info='MEM TOP 10进程')
         mem_top.insert(2, top[2])
-        sysctl = i_conn.remote_exec("sysctl -p | sed 's/=//g'", info = '非默认内核参数')
+        sysctl = i_conn.remote_exec("sysctl -p | sed 's/=//g'", info='非默认内核参数')
         sysctl.insert(2, ['参数', '值'])
         for index, item in enumerate(sysctl):
-            if len(item) > 2 and type(item) is types.ListType:
+            if len(item) > 2 and isinstance(item, list):
                 t = ' | '.join(str(n) for n in item[1:])
                 item = item[:1]
                 item.append(t)
                 sysctl[index] = item
 #                print item
-        startups = i_conn.remote_exec('chkconfig --list', info = '启动服务列表')
+        startups = i_conn.remote_exec('chkconfig --list', info='启动服务列表')
         startups.insert(2, ['程序名', 'init 0', 'init 1', 'init 2', 'init 3', 'init 4', 'init 5', 'init 6'])
         #以此结构组织输出格式
         appendlist = [['基本信息'],
@@ -292,15 +304,20 @@ class OsInfo(object):
         outlist = self.outlist
         for item in appendlist:
             outlist.append(item)
+
     def aix_func(self):
+        """
+
+        """
+
         def parse_lsdev(command, info=None):
             out = i_conn.remote_exec(command, info=info)
             out.insert(2, ['名称', '状态', '插槽', '说明'])
             for index, item in enumerate(out[3:]):
                 if not re.match('\d', item[2]):
-                    out[index+3].insert(2, '#')
-                out[index+3][3] = ' '.join(str(n) for n in item[3:])
-                out[index+3] = out[index+3][:4]
+                    out[index + 3].insert(2, '#')
+                out[index + 3][3] = ' '.join(str(n) for n in item[3:])
+                out[index + 3] = out[index + 3][:4]
             return out
 
         def parse_lsattr(command, info):
@@ -308,11 +325,11 @@ class OsInfo(object):
             attr.insert(2, ['属性', '值', '描述', '可设置'])
             for index, item in enumerate(attr[3:]):
                 #合并描述
-                attr[index+3][2] = ' '.join(str(n) for n in item[2:-1])
+                attr[index + 3][2] = ' '.join(str(n) for n in item[2:-1])
                 #把最后一列调到前面
-                attr[index+3][3] = item[-1]
+                attr[index + 3][3] = item[-1]
                 #删掉多余的元素
-                attr[index+3] = attr[index+3][:4]
+                attr[index + 3] = attr[index + 3][:4]
             return attr
         #basic_func()
         i_conn = self.i_conn
@@ -323,7 +340,7 @@ class OsInfo(object):
         #manufacturer = 'IBM'
         sn = i_conn.exec_single('uname -u')
         self.basic_list = ['', '', ['主机名', 'IP 地址', '操作系统', 'CPU架构', '快照时间', '序列号'],
-            [hostname, ipaddr,self.os_type, arch, __time__, sn]]
+            [hostname, ipaddr, self.os_type, arch, __time__, sn]]
         sys_ver = i_conn.exec_single('oslevel -s')
         xlC_ver = i_conn.exec_single("/usr/vacpp/bin/xlC -qversion | tail -n 1 | awk '{print $2}'")
         java_dir = i_conn.remote_exec('ls -d /usr/java*')
@@ -332,7 +349,6 @@ class OsInfo(object):
             java_ver += n + ', '
         verlist = ['', '版本信息', ['系统版本', '编译器版本', '系统中的Java'],
                    [sys_ver, xlC_ver, java_ver]]
-
 
         syslist = parse_lsattr('lsattr -El sys0', info='核心信息')
 
@@ -345,7 +361,7 @@ class OsInfo(object):
         mem_usage = i_conn.remote_exec('svmon -G | head -n 2', info='内存使用率(单位MB)')
         mem_usage[2].insert(0, '#')
         for index, value in enumerate(mem_usage[3][1:-1]):
-            mem_usage[3][index+1] = int(value)*4/1024
+            mem_usage[3][index + 1] = int(value) * 4 / 1024
         ps_usage = i_conn.remote_exec('lsps -s', x=1, info='交换空间')
         ps_usage.insert(2, ['总大小', '剩余百分比'])
         #mpstat = i_conn.remote_exec('mpstat 2 4 | grep -v -', x=3, info='CPU使用率')
@@ -365,50 +381,72 @@ class OsInfo(object):
         del dflist[2][-2]
         dflist[2].insert(-1, 'Mounted on')
         #网络信息
+        #TODO: IP
+        #ifconfig = i_conn.remote_exec('ifconfig -a')
 
-        adapter = parse_lsdev('lsdev -Cc adapter', info='适配器信息（网卡，HBA卡等）')
+        ifattr = parse_lsattr('lsattr -El en0', info='网卡属性')
+        router = i_conn.remote_exec('netstat -rn | grep en', info='内核路由表')
+        router.insert(2, ['Destination', 'Gateway', 'Flags', 'Refs', 'Use', 'If', 'Exp', 'Groups'])
+        #telnet引入的问题，命令输出不能有#，如果有，要替换掉
+        #netstat = i_conn.remote_exec("netstat -in | sed 's/#/$/g' | grep en", info='网卡工作状态')
+        adapter = parse_lsdev('lsdev -Cc adapter', info='适配器信息')
+        cpu_args = i_conn.remote_exec("schedo -F -a | sed 's/=//g'", info='CPU参数')
+        cpu_args.insert(2, ['参数', '值'])
+        io_args = i_conn.remote_exec("ioo -F -a | sed 's/=//g'", info='I/O参数')
+        io_args.insert(2, ['参数', '值'])
+        no_args = i_conn.remote_exec("no -F -a | sed 's/=//g'", info='网络参数')
+        no_args.insert(2, ['参数', '值'])
         #以此结构组织输出格式
         appendlist = [['基本信息'],
             self.basic_list, verlist, syslist,
             ['CPU/内存'],
-            cpulist, mem_usage, ps_usage, cpu_usage,vmstat,
+            cpulist, memory, mem_usage, ps_usage, cpu_usage, vmstat,
             ['存储信息'],
             hdisk, dflist, iostat,
             ['网络信息'],
-
+            ifattr, router,
             ['拓展信息'],
-            adapter
+            adapter,
+            ['系统参数']
         ]
 
         self.outlist = []
         outlist = self.outlist
         for item in appendlist:
             outlist.append(item)
+
     def hp_func(self):
         basic_func()
+
     def sol_func(self):
         basic_func()
+
+
 def get_db_info(self):
     pass
 
 #info = OsInfo()
+
 
 def gen_table(com_out_list):
     '''生成表格
 
     参数为命令输出解析完的list'''
     rpt_table = p() << table(width='500', border='1')
+
     def thb(args):
         return th(args, cl='awrbg', colspan='0')
+
     def tdc(args):
         return td(args, cl='awrc', align='left', colspan='0')
+
     def tdnc(args):
         return td(args, cl='awrnc', align='left', colspan='0')
     rpt_table = rpt_table
     tro = tr()
     table_head = com_out_list[2]
     for item in table_head:
-       tro  << thb(item)
+        tro << thb(item)
     rpt_table << tro
     tro = tr()
     table_data = com_out_list[3:]
@@ -424,10 +462,12 @@ def gen_table(com_out_list):
 #            for item in line:
 #                tro << tdc(item)
             c_or_nc = True
-        #TODO: tr标签闭合不正确
+        #FIXME: tr标签闭合不正确
         tro << tr()
     rpt_table << tro
     return rpt_table
+
+
 def gen_html(info):
     '''生成html'''
     print u'正在生成html报表'
@@ -460,7 +500,7 @@ def gen_html(info):
             if line[0]:
                 report << br()
                 report << liawr << a(line[0],
-                                     href='http://man.he.net/?topic='+line[0].split()[0]+'&section=all')
+                                     href='http://man.he.net/?topic=' + line[0].split()[0] + '&section=all')
                 liawr = li(cl='awr')
             report << br()
             report << gen_table(line)
@@ -470,12 +510,15 @@ def gen_html(info):
     rpt_file.flush()
     rpt_file.close()
     print u'报表生成完成：' + 'reports/ta_report_' + __now__ + '.html'
+
+
 def main():
     connection = Connector()
     #connection.remote_init()
+#    connection.check_rpm('dmidecode')
+#    connection.check_rpm('sysstat')
     info = OsInfo(connection)
     info.choose()
     gen_html(info)
 if __name__ == '__main__':
     main()
-
